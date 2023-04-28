f31421351b8412cbd91c373c6bd53e4145332b4b
Luke <lukezhu0314@gmail.com>
2023-04-27 20:55:17 -0700

    added more softlinks

--- install/install-nvim.sh
+++ install/install-nvim.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Create all necessary folder for neovim
+# note VIMCONFIG is set to ~/.config/nvim
 if [ ! -d "$VIMCONFIG" ]
 	then
 		mkdir "$VIMCONFIG"
@@ -8,3 +9,9 @@ if [ ! -d "$VIMCONFIG" ]
 		curl -fLo ~/dotfiles/nvim/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+ln -sf "$DOTFILES_DIR/nvim/init.vim" "$VIMCONFIG"
+ln -sf "$DOTFILES_DIR/nvim/ftplugin" "$VIMCONFIG"
+ln -sf "$DOTFILES_DIR/nvim/autoload" "$VIMCONFIG"
+
+mkdir -p "$VIMCONFIG/plugged"
